df3c5ec63311ace165812b5cd282074ca9fc1ed8
KYOOKIM <dev.kyoomin@gmail.com>
2024-04-09 13:52:19 +0900

    Update model_server

--- 4_model_server/server.py
+++ 4_model_server/server.py
@@ -10,59 +10,38 @@ import uvicorn
 import model
 import utils
 
-yolo_model: YOLO = None
+yolo_model: YOLO = model.load_model()
 
 class ImagePayload(BaseModel):
     image: str
 
-def on_startup():
-    global yolo_model
-    logging.basicConfig(level=logging.INFO)
-    yolo_model = model.load_model()
-    if torch.cuda.is_available():
-        yolo_model = yolo_model.to("cuda")
-
-def on_shutdown():
-    global yolo_model
-    del yolo_model
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    on_startup()
-    yield
-    on_shutdown()
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 @app.get("/")
 def health_check():
     return {"status": 200,
             "message": "Server is running"}
 
+
 @app.post("/inference/base64")
-async def inference_endpoint_base64(image_payload: ImagePayload):
-    try:
-        global yolo_model
-        img = utils.base64_to_pillow_image(image_payload.image)
-        result = model.inference(yolo_model, img)
-        return {"status":200,"result": result}
-    except Exception as e:
-        logging.error(f"Inference Error: {e}")
-        return {"status":500,"error": str(e)}
+def inference_endpoint_base64(image_payload: ImagePayload):
+    global yolo_model
+    img = utils.base64_to_pillow_image(image_payload.image)
+
+    result = model.inference(yolo_model, img)
+
+    return {"status":200,"result": result}
+
 
 @app.post("/inference")
 async def inference_endpoint(file: UploadFile = File()):
-    try:
-        global yolo_model
-        contents = await file.read()
-        image = Image.open(BytesIO(contents))
-        result = model.inference(yolo_model, image)
-        return {"status":200,"result": result}
-    except Exception as e:
-        logging.error(f"Inference Error: {e}")
-        return {"status":500,"error": str(e)}
+    global yolo_model
+    contents = await file.read()
+    image = Image.open(BytesIO(contents))
+
+    result = model.inference(yolo_model, image)
+
+    return {"status":200,"result": result}
 
 
 if __name__ == "__main__":
